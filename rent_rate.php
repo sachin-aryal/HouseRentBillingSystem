0636dff93a3a9b9db6d032d77d3fa7ed353ccb9e
Ashish BIkram <bikram.ashish@gmail.com>
2018-01-16 11:00:12 +0545

    rent rate

---
+++ rent_rate.php
@@ -0,0 +1,65 @@
+<?php
+include '_header.php';
+if(isset($_POST['save_rate'])){
+    $id=1;
+    $rate=$_POST['electricity_rate'];
+    $stmt=$conn->prepare('update electricity_charge set rate=? where id=?');
+    $stmt->bind_param('ii',$rate,$id);
+    if($stmt->execute()){
+        header("Location:rent_rate.php");
+    }
+    header("Location:rent_rate.php");
+
+}
+
+$charge=getElectricityRate($conn);
+?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-4 col-md-4">
+            <h2>Electricity Rate</h2>
+            <?php if(isset($_POST['edit_rate'])){?>
+                <form method="post" action="rent_rate.php">
+                    <div class="form-group">
+                        <label for="electricity_rate">Electricity Rate:</label>
+                        <input type="number" name="electricity_rate" class="form-control" id="electricity_rate" value="<?php echo $charge['rate'] ?>">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary" name="save_rate" >Update</button>
+
+                </form>
+
+           <?php  }else{ ?>
+                <table id="rent-list" class="display">
+                    <thead>
+                    <tr>
+                        <th>SN</th>
+                        <th>Rate</th>
+                        <th>Action</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                            <td><?php echo 1 ?></td>
+                            <td>NRs <?php
+                            echo $charge['rate'];
+                            ?></td>
+
+                            <td>
+                                <form method="post" action="rent_rate.php">
+                                    <button type="submit" class="btn btn-primary" name="edit_rate" >Edit</button>
+
+                                </form>
+                            </td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            <?php } ?>
+    </div>
+</div>
